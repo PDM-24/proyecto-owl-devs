02f10d1ac0c3ddd82761ba167b0fcf034424e768
Calderon0723 <00215818@uca.edu.sv>
2024-05-08 18:12:02 -0600

    Add: Fonts & Colors :lipstick:

--- app/src/main/java/com/owldevs/taskme/ui/theme/Color.kt
+++ app/src/main/java/com/owldevs/taskme/ui/theme/Color.kt
@@ -2,9 +2,14 @@ package com.owldevs.taskme.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+/*Paletade colores MODO OSCURO*/
+val AzulMarino = Color(0xFF121F34)
+val Turquesa = Color(0xFFAEFFF6)
+val AzulVerdoso = Color(0xFF007193)
+val NaranjaIntenso = Color(0xFFFFAA3F)
+val RojoRosa = Color(0xFFFE5260)
+val Blanco = Color(0xFFFFFFFF)
+val GrisOscuro = Color(0xFF333333)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
--- app/src/main/java/com/owldevs/taskme/ui/theme/Theme.kt
+++ app/src/main/java/com/owldevs/taskme/ui/theme/Theme.kt
@@ -16,9 +16,18 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Turquesa,
+    secondary = AzulVerdoso,
+    background = AzulMarino,
+    tertiary = NaranjaIntenso,
+    error = RojoRosa,
+
+
+    onPrimary = GrisOscuro,
+    onSecondary = Blanco,
+    onBackground = Blanco,
+    onTertiary = GrisOscuro,
+    onError = Blanco,
 )
 
 private val LightColorScheme = lightColorScheme(
